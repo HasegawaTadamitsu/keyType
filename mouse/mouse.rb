cea29c5fd968b6347215195b3571046f90fd9ad6
hase <hasegawa.tadamitsu@gmail.com>
2012-10-30 06:28:57 +0900

    mouse

--- mouse/mouse.rb
+++ mouse/mouse.rb
@@ -13,9 +13,10 @@ HEIGHT=800
 screen =SDL::Screen.open( WIDTH, HEIGHT, 16, SDL::NOFRAME )
 
 SDL::TTF.init
-@font_small = SDL::TTF.open(TRUE_TYPE_FONT, 250, 0)
-@font_big = SDL::TTF.open(TRUE_TYPE_FONT, 450, 0)
+@@font_small = SDL::TTF.open(TRUE_TYPE_FONT, 250, 0)
+@@font_big = SDL::TTF.open(TRUE_TYPE_FONT, 450, 0)
 SDL::Mouse.hide
+@buffer = Array.new
 
 SAMPLE=["ぞうさん","しまじろう","お父さん","お母さん",
      "げんこつやま",
@@ -23,16 +24,43 @@ SAMPLE=["ぞうさん","しまじろう","お父さん","お母さん",
      "０","１","２","３","４","５",
      "６","７","８","９","１０"]
 
-NUMBER=[ "０","１","２","３","４","５",
-         "６","７","８","９","１０"]
+NUMBER=[ "0","1","2","3","4","5",
+         "6","7","8","9","10"]
+MSG=NUMBER
+#MSG=SAMPLE
 
-PLUS=1.upto 9 do |x|
-      1.upto 9 do |y|
-    "#{x} + #{y}"
-    end
+class String 
+  def draw screen
+  w, h = @@font_big.textSize(self)
+  if w < WIDTH 
+    set_x = WIDTH/2 - w/2
+    set_y = HEIGHT/2 - h/2
+    @@font_big.draw_solid_utf8(screen,self,set_x, set_y ,255,255,255)
+    return
+  end
+  msg1 = self[0..self.length/2]
+  msg2 = self[self.length/2..-1]
+  w, h = @@font_small.textSize(msg1)
+  set_x = WIDTH/2 - w/2
+  set_y = HEIGHT/10*3 - h/2
+  @@font_small.draw_solid_utf8(screen,msg1,set_x, set_y ,255,255,255)
+  w, h = @@font_small.textSize(msg2)
+  set_x = WIDTH/2 - w/2
+  set_y = HEIGHT/10*6 - h/2
+  @@font_small.draw_solid_utf8(screen,msg2,set_x, set_y ,255,255,255)
+  end
 end
 
-MSG=NUMBER
+class Array
+  def mypush val
+#  binding.pry
+    if 10 < self.size
+      self.shift
+    end
+    self.push val
+  end
+end
+
 
 def random_msg
  return MSG[rand(MSG.length)] 
@@ -40,42 +68,33 @@ end
 
 def mouse_button
   dm, dm, lbutton, mbutton,rbutton = SDL::Mouse.state
-#  binding.pry
   if lbutton == @last_lbutton and 
-     mbutton == @last_mbutton and 
      rbutton == @last_rbutton then
      return @last_message 
   end
   @last_lbutton = lbutton
-  @last_mbutton = mbutton
   @last_rbutton = rbutton
-  if lbutton == false  and  mbutton == false and rbutton == false
+  if (lbutton == false  and  rbutton == false) or
+     (lbutton == true   and  rbutton == true ) 
     @last_message = ""
-  else
-    @last_message = random_msg
+    return @last_message
   end
-  return @last_message
+  if  rbutton == true
+    @last_message = random_msg
+    @buffer.mypush @last_message
+    return @last_message
+  end
+  if  lbutton == true
+    msg =  @buffer.pop
+    if msg.nil?
+      msg=""
+    end
+    @last_message = msg
+     return @last_message
+  end
 end
 
-def draw_textbox screen,msg
-  w, h = @font_big.textSize(msg)
-  if w < WIDTH 
-    set_x = WIDTH/2 - w/2
-    set_y = HEIGHT/2 - h/2
-    @font_big.draw_solid_utf8(screen,msg,set_x, set_y ,255,255,255)
-    return
-  end
-  msg1 = msg[0..msg.length/2]
-  msg2 = msg[msg.length/2..-1]
-  w, h = @font_small.textSize(msg1)
-  set_x = WIDTH/2 - w/2
-  set_y = HEIGHT/10*3 - h/2
-  @font_small.draw_solid_utf8(screen,msg1,set_x, set_y ,255,255,255)
-  w, h = @font_small.textSize(msg2)
-  set_x = WIDTH/2 - w/2
-  set_y = HEIGHT/10*6 - h/2
-  @font_small.draw_solid_utf8(screen,msg2,set_x, set_y ,255,255,255)
-end
+
 
 @last_lbutton=false
 @last_mbutton=false
@@ -101,7 +120,7 @@ loop do
   screen.fillRect( 0, 0, WIDTH, HEIGHT, [ 0, 0, 0 ] )
 
   if print_msg !=""
-    draw_textbox(screen,print_msg)
+    print_msg.draw screen
   end
 
   timer.wait_frame do
